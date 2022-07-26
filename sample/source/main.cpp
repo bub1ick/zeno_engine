acfbfd9d386a50ad87df2128a0c84ad79a45be3e
bub1ick <smirnovkostya@inbox.ru>
2022-07-26 21:58:58 +0300

    replaced windows pp define with portable one

--- sample/source/main.cpp
+++ sample/source/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <zeno.hpp>
 
-#ifdef _WIN32
+#ifdef ZENO_WINDOWS
 int32_t APIENTRY WinMain(
     _In_ HINSTANCE instance, _In_opt_ HINSTANCE prev_instance, _In_ char* command_line, _In_ int32_t show_command
 )
