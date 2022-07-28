509a3191ae12425785a71a8c5d2cad24595516e4
bub1ick <smirnovkostya@inbox.ru>
2022-07-28 16:50:21 +0300

    create swap chain

--- zeno/zenocore/source/application/application.cpp
+++ zeno/zenocore/source/application/application.cpp
@@ -90,20 +90,27 @@ void application_t::run()
 
         device_context->Release();
 
-        DXGI_SWAP_CHAIN_DESC descriptor = {};
-        descriptor.Windowed             = TRUE;
-        descriptor.BufferCount          = 2;
-        descriptor.BufferDesc.Format    = DXGI_FORMAT_B8G8R8A8_UNORM;
-        descriptor.BufferUsage          = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-        descriptor.SampleDesc.Count     = 1;
-        descriptor.SampleDesc.Quality   = 0;
-        descriptor.SwapEffect           = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-        descriptor.OutputWindow         = _window->get_window_handle();
+        DXGI_SWAP_CHAIN_DESC1 descriptor = {};
 
-        IDXGIDevice4*  dxgi_device4  = nullptr;
-        IDXGIAdapter*  dxgi_adapter  = nullptr;
-        IDXGIAdapter4* dxgi_adapter4 = nullptr;
-        IDXGIFactory7* dxgi_factory7 = nullptr;
+        descriptor.Width       = 0;
+        descriptor.Height      = 0;
+        descriptor.Format      = DXGI_FORMAT_R16G16B16A16_FLOAT;
+        descriptor.Stereo      = false;
+        descriptor.SampleDesc  = {1, 0};
+        descriptor.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        descriptor.BufferCount = 2;
+        descriptor.Scaling     = DXGI_SCALING_ASPECT_RATIO_STRETCH;
+        descriptor.SwapEffect  = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+        descriptor.AlphaMode   = DXGI_ALPHA_MODE_PREMULTIPLIED;
+        descriptor.Flags       = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH | DXGI_SWAP_CHAIN_FLAG_FULLSCREEN_VIDEO |
+                           DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING;
+
+        IDXGIDevice4*    dxgi_device4    = nullptr;
+        IDXGIAdapter*    dxgi_adapter    = nullptr;
+        IDXGIAdapter4*   dxgi_adapter4   = nullptr;
+        IDXGIFactory7*   dxgi_factory7   = nullptr;
+        IDXGISwapChain1* dxgi_swapchain1 = nullptr;
+        IDXGISwapChain4* dxgi_swapchain4 = nullptr;
 
         result = device5->QueryInterface<IDXGIDevice4>(&dxgi_device4);
         if (FAILED(result))
@@ -120,10 +127,28 @@ void application_t::run()
         result = dxgi_adapter->QueryInterface<IDXGIAdapter4>(&dxgi_adapter4);
         if (FAILED(result))
         {
-            throw std::runtime_error("Could't query IDXGIAdapter3 from IDXGIAdapter");
+            throw std::runtime_error("Couldn't query IDXGIAdapter3 from IDXGIAdapter.");
         }
 
-        dxgi_adapter4->GetParent(IID_IDXGIFactory7, reinterpret_cast<void**>(dxgi_factory7));
+        result = dxgi_adapter4->GetParent(IID_IDXGIFactory7, reinterpret_cast<void**>(&dxgi_factory7));
+        if (FAILED(result))
+        {
+            throw std::runtime_error("Couldn't get DXGIFactory parent of DXGI Adapter.");
+        }
+
+        result = dxgi_factory7->CreateSwapChainForHwnd(
+            device5, _window->get_window_handle(), &descriptor, nullptr, nullptr, &dxgi_swapchain1
+        );
+        if (FAILED(result))
+        {
+            throw std::runtime_error("Couldn't create Swap Chain.");
+        }
+
+        result = dxgi_swapchain1->QueryInterface<IDXGISwapChain4>(&dxgi_swapchain4);
+        if (FAILED(result))
+        {
+            throw std::runtime_error("Couldn't query IDXGISwapChain4 from IDXGISwapChain1");
+        }
     }
     catch (std::exception& exception)
     {
