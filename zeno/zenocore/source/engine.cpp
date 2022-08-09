eccc3b572ff55b1e5caf029443ecd1b9b4cff781
bub1ick <smirnovkostya@inbox.ru>
2022-08-09 03:24:35 +0300

    made renderer stack allocated

--- zeno/zenocore/source/engine.cpp
+++ zeno/zenocore/source/engine.cpp
@@ -4,40 +4,18 @@ namespace zeno
 {
 
 engine_t::engine_t(const char* window_name, int32_t pos_x, int32_t pos_y, int32_t size_x, int32_t size_y)
+    : _renderer(window_name, pos_x, pos_y, size_x, size_y)
 {
-    try
-    {
-        //  Try getting the application instance
-        HINSTANCE instance = GetModuleHandleA(nullptr);
-        if (!instance)
-        {
-            throw std::runtime_error(
-                "Could not get .exe instance handle. To avoid this issue, please use the full constructor."
-            );
-        }
-
-        _renderer = new renderer_t(window_name, instance, pos_x, pos_y, size_x, size_y);
-    }
-    catch (std::exception& exception)
-    {
-        std::cout << exception.what();
-    }
-}
-
-engine_t::~engine_t()
-{
-    delete _renderer;
 }
 
 void engine_t::run()
 {
-
-    _renderer->initialize();
+    _renderer.initialize();
 
     MSG message;
     while (true)
     {
-        if (GetMessageA(&message, _renderer->get_window()->get_window_handle(), 0, 0) != 0)
+        if (GetMessageA(&message, _renderer.get_window()->get_window_handle(), 0, 0) != 0)
         {
             TranslateMessage(&message);
             DispatchMessageA(&message);
