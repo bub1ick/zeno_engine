b542ad75ce9611cc01e8f8039a779cffe8a14c6f
bub1ick <smirnovkostya@inbox.ru>
2022-08-15 13:57:03 +0300

    moved window handling to plat module

--- zeno/zenoplat/headers/platform.hpp
+++ zeno/zenoplat/headers/platform.hpp
@@ -2,5 +2,12 @@
 
 namespace zeno
 {
-void platform_loop(const std::function<void()>& engine_loop);
+namespace plat
+{
+    window_t* create_platform_window(
+        const char* in_window_title, int32_t in_pos_x, int32_t in_pos_y, int32_t in_size_x, int32_t in_size_y
+    );
+
+    void platform_loop(const std::function<void()>& in_engine_loop);
+}
 }  //  namespace zeno
