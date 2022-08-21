dcbe7177ab1c02a3c534bd2658ad8e80804e4d0c
bub1ick <smirnovkostya@inbox.ru>
2022-08-22 01:15:55 +0300

    removed sdl

--- third_party/third_party.hpp
+++ third_party/third_party.hpp
@@ -1,7 +1,4 @@
 #pragma once
 
-//  SDL libs
-#include "SDL/include/SDL.h"
-
 // glm headers
 #include "glm/glm/glm.hpp"
--- zeno/zenocore/source/engine.cpp
+++ zeno/zenocore/source/engine.cpp
@@ -7,22 +7,13 @@ engine_t::engine_t(const char* in_window_name, int32_t in_pos_x, int32_t in_pos_
     m_window_ptr = plat::create_platform_window(in_window_name, in_pos_x, in_pos_y, in_size_x, in_size_y);
 
     m_renderer_ptr = new dx11::renderer_t(m_window_ptr);
-
-    SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
-
-    SDL_Window*   window;
-
-    window = SDL_CreateWindow(
-        "test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI
-    );
 }
 
 void engine_t::run()
 {
     plat::platform_loop(
-        [this] {
-
+        [this]
+        {
         }
     );
 
