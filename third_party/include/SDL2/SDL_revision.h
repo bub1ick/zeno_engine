74867fc5ebe27cb858396671efe86f3d53ce1811
bub1ick <smirnovkostya@inbox.ru>
2022-09-19 00:34:23 +0300

    removed third party libs

--- third_party/include/SDL2/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@b204db1e6b120bd28ab4f55590949912293a79b8"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
