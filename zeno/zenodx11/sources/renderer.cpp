555bd62cfeecdc992796d9db7fc93c1db6a9e300
bub1ick <smirnovkostya@inbox.ru>
2022-11-29 21:35:13 +0300

    update

--- zeno/zenodx11/sources/renderer.cpp
+++ zeno/zenodx11/sources/renderer.cpp
@@ -35,11 +35,11 @@ renderer_t::renderer_t(HWND window_handle)
     }
 
     DXGI_SWAP_CHAIN_DESC1 swapchain_descriptor {};
-    swapchain_descriptor.Width       = 0;                           //  determine by the set window size
-    swapchain_descriptor.Height      = 0;                           //  determine by the set window size
-    swapchain_descriptor.Format      = DXGI_FORMAT_R8G8B8A8_UNORM;  //  FIXME some random format that is supported by flip swap chain
-    swapchain_descriptor.Stereo      = false;                       //  true is for VR-like things (two screens rendering at the same time)
-    swapchain_descriptor.SampleDesc  = {1, 0};                      //  disable MSAA
+    swapchain_descriptor.Width       = 0;                               //  determine by the set window size
+    swapchain_descriptor.Height      = 0;                               //  determine by the set window size
+    swapchain_descriptor.Format      = DXGI_FORMAT_R16G16B16A16_FLOAT;  //  some random format that is supported by flip swap chain
+    swapchain_descriptor.Stereo      = false;                           //  true is for VR-like things (two screens rendering at the same time)
+    swapchain_descriptor.SampleDesc  = {1, 0};                          //  disable MSAA
     swapchain_descriptor.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT;  //  how can the swapchain be used
     swapchain_descriptor.BufferCount = 2;                                                          //  we want the front and the back buffer (sums up to 2)
     swapchain_descriptor.Scaling     = DXGI_SCALING_NONE;                                          //  stretch preserving the original rendered aspect ratio
@@ -47,11 +47,16 @@ renderer_t::renderer_t(HWND window_handle)
     swapchain_descriptor.AlphaMode   = DXGI_ALPHA_MODE_UNSPECIFIED;                                //  FIXME IDK what is this
     swapchain_descriptor.Flags       = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
-    uint32_t number_of_display_modes = 0;
-    m_dxgi.monitor->GetDisplayModeList1(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_SCALING, &number_of_display_modes, nullptr);
-    DXGI_MODE_DESC1* display_modes = new DXGI_MODE_DESC1 [number_of_display_modes];
-    m_dxgi.monitor->GetDisplayModeList1(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_SCALING, &number_of_display_modes, display_modes);
+    //  info on the dxgi format topic:
+    //      - https://learn.microsoft.com/en-us/windows/win32/direct3ddxgi/converting-data-color-space
+    //      - https://developer.nvidia.com/gpugems/gpugems3/part-iv-image-effects/chapter-24-importance-being-linear
 
+
+    //  get display modes and take the best one (highest resolution and refresh rate)
+    uint32_t number_of_display_modes = 0;
+    m_dxgi.monitor->GetDisplayModeList1(DXGI_FORMAT_R16G16B16A16_FLOAT, DXGI_ENUM_MODES_SCALING, &number_of_display_modes, nullptr);
+    DXGI_MODE_DESC1* display_modes = new DXGI_MODE_DESC1 [number_of_display_modes];
+    m_dxgi.monitor->GetDisplayModeList1(DXGI_FORMAT_R16G16B16A16_FLOAT, DXGI_ENUM_MODES_SCALING, &number_of_display_modes, display_modes);
     DXGI_MODE_DESC1 best_display_mode = display_modes [number_of_display_modes - 1];
 
     //  DEBUG print display modes
@@ -79,31 +84,27 @@ renderer_t::renderer_t(HWND window_handle)
 
     DXGI_SWAP_CHAIN_FULLSCREEN_DESC swapchain_fullscreen_descriptor {};
 
-    swapchain_fullscreen_descriptor.RefreshRate      = best_display_mode.RefreshRate;
-    swapchain_fullscreen_descriptor.ScanlineOrdering = best_display_mode.ScanlineOrdering;
-    swapchain_fullscreen_descriptor.Scaling          = DXGI_MODE_SCALING_CENTERED;
-    swapchain_fullscreen_descriptor.Windowed         = false;
+    swapchain_fullscreen_descriptor.RefreshRate      = best_display_mode.RefreshRate;       //  get the highest refresh rate from the monitor
+    swapchain_fullscreen_descriptor.ScanlineOrdering = best_display_mode.ScanlineOrdering;  //  get scanline ordering from the monitor
+    swapchain_fullscreen_descriptor.Scaling          = DXGI_MODE_SCALING_CENTERED;          //  FIXME never use scaling (for now)
+    swapchain_fullscreen_descriptor.Windowed         = false;                               //  start the application in fullscreen
 
-    //  TODO: implement fullscreen swapchain (https://learn.microsoft.com/en-us/windows/win32/api/dxgi1_2/nf-dxgi1_2-idxgioutput1-getdisplaymodelist1)
     //  this is how to get the refresh rate of the monitor. the modes are resolutions combined with refresh rate and etc.
     m_result = m_dxgi.factory->CreateSwapChainForHwnd(
         m_dxgi.device, window_handle, &swapchain_descriptor, &swapchain_fullscreen_descriptor, nullptr, reinterpret_cast<IDXGISwapChain1**>(&m_swapchain)
     );
-
     if (FAILED(m_result))
     {
         std::cout << "Couldn't create swapchain for the window:" << std::flush;
+    }
 
-        switch (m_result)
+    // DEBUG
+    {
+        m_swapchain->GetDesc1(&swapchain_descriptor);
+
         {
-            case DXGI_ERROR_INVALID_CALL:
-                std::cout << "\tinvalid params" << std::endl;
-                break;
-            case E_OUTOFMEMORY:
-                std::cout << "\tmemory is unavailable to complete the operation" << std::endl;
-                break;
-            default:
-                std::cout << "\t other shit" << std::endl;
+            std::cout << std::endl << std::endl;
+            std::cout << swapchain_descriptor.Width << std::endl;
         }
     }
 
