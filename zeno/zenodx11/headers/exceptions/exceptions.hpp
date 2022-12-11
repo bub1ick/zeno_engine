3c133bbb29c6ce30d40f0b9f90e2cb3ae35bcd15
bub1ick <smirnovkostya@inbox.ru>
2022-12-11 04:45:44 +0300

    added exception handling to dxgi part

---
+++ zeno/zenodx11/headers/exceptions/exceptions.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace zeno::dx11
+{
+class dxgi_exception_t
+{
+public:
+    dxgi_exception_t(std::string_view in_error_string, HRESULT in_result);
+    ~dxgi_exception_t();
+    dxgi_exception_t& operator=(const dxgi_exception_t& in_that);
+    std::string_view  get_error_message() const;
+
+protected:
+    std::string m_error_message;
+    HRESULT     m_result;
+};
+}  //  namespace zeno::dx11
