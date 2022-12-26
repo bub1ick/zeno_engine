c574ae1bcb082c92d3bdf67fc190ff7077923e8e
bub1ick <smirnovkostya@inbox.ru>
2022-12-27 02:23:01 +0300

    added necessary defines for the client apps

---
+++ zeno/zeno_user_difines.hpp
@@ -0,0 +1,6 @@
+// This header file must be included in the client apps.
+
+// tinygltf defines
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
