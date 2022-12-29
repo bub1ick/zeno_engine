f23ce22a4a4dc7d5483b7184053ff8ee06bd4a50
bub1ick <smirnovkostya@inbox.ru>
2022-12-29 14:22:01 +0300

    added support for c++14 in tinygltf

--- zeno/zeno_user_difines.hpp
+++ zeno/zeno_user_difines.hpp
@@ -1,6 +1,7 @@
-// This header file must be included in the client apps.
+//  This header file must be included in the client apps.
 
-// tinygltf defines
+//  tinygltf defines
 #define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_USE_CPP14
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
