f3c7984f979367d5ddb37cd7c8225cdb89e35208
bub1ick <smirnovkostya@inbox.ru>
2023-01-11 04:09:37 +0300

    a working exception handling

--- zeno/zenogfx/headers/exceptions.hpp
+++ zeno/zenogfx/headers/exceptions.hpp
@@ -2,15 +2,24 @@
 
 namespace zeno::gfx
 {
-class dxgi_exception_t
+class dx_exception_t
 {
 public:
-    dxgi_exception_t(std::string_view in_error_string, HRESULT in_result);
-    ~dxgi_exception_t();
-    void             operator=(const dxgi_exception_t& in_that);
+    enum category_e
+    {
+        unspecified,
+        d3d11,
+        dxgi
+    };
+
+    dx_exception_t(std::string_view in_error_string, HRESULT in_result, category_e exception_category);
+    ~dx_exception_t() = default;
+    void        operator=(const dx_exception_t& in_that);
     std::string get_error_message() const;
 
-protected:
+private:
+    void category_format(category_e exception_category);
+
     std::string m_error_message;
     HRESULT     m_result;
 };
--- zeno/zenogfx/sources/exceptions.cpp
+++ zeno/zenogfx/sources/exceptions.cpp
@@ -2,27 +2,41 @@
 
 namespace zeno::gfx
 {
-dxgi_exception_t::dxgi_exception_t(std::string_view in_error_string, HRESULT in_result)
-    : m_error_message(in_error_string),
-      m_result {in_result}
+dx_exception_t::dx_exception_t(std::string_view in_error_string, HRESULT in_result, category_e exception_category)
+    : m_result {in_result}
 {
+    category_format(exception_category);
+    m_error_message += in_error_string;
 }
 
-dxgi_exception_t::~dxgi_exception_t()
-{
-    //  nothing to deinitialize
-}
-
-void dxgi_exception_t::operator=(const dxgi_exception_t& in_that)
+void dx_exception_t::operator=(const dx_exception_t& in_that)
 {
     this->m_error_message = in_that.get_error_message();
 }
 
-std::string dxgi_exception_t::get_error_message() const
+std::string dx_exception_t::get_error_message() const
 {
     std::stringstream error_code;
     error_code << std::hex << m_result;  //  convert result value to hex format
-    return m_error_message + '\t' + error_code.str();
+    return m_error_message + "\n\t-Error code:" + error_code.str();
+}
+
+void dx_exception_t::category_format(category_e exception_category)
+{
+    switch (exception_category)
+    {
+        case category_e::d3d11:
+            m_error_message = "Direct3D exception caught:\n\t-";
+            break;
+
+        case category_e::dxgi:
+            m_error_message = "DXGI exception caught:\n\t-";
+            break;
+
+        default:
+            m_error_message = "Unspecified DirectX exception caught:\n\t-";
+            break;
+    }
 }
 
 }  //  namespace zeno::gfx
