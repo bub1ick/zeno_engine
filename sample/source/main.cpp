3b69d3a6d280fe243000e26e0c4c2c38c105dcb3
bub1ick <smirnovkostya@inbox.ru>
2023-01-12 14:16:50 +0300

    macro changes

--- sample/source/main.cpp
+++ sample/source/main.cpp
@@ -1,11 +1,20 @@
-﻿#include <zeno.hpp>
+﻿#define ZENO_MAIN
+#include <zeno.hpp>
 
 #ifdef ZENO_WINDOWS
-int32_t APIENTRY WinMain(
-    _In_ HINSTANCE instance, _In_opt_ HINSTANCE prev_instance, _In_ char* command_line, _In_ int32_t show_command
-)
+
+int32_t APIENTRY WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev_instance, _In_ char* command_line, _In_ int32_t show_command)
 {
     zeno::engine_t engine("test");
     return engine.run();
 }
+
+#elif
+
+int32_t main(int32_t agrc, char** argv)
+{
+    zeno::engine_t engine("test");
+    return engine.run();
+}
+
 #endif
--- zeno/zeno_defines.hpp
+++ zeno/zeno_defines.hpp
@@ -14,22 +14,23 @@
 #    define ZENO_API
 #endif
 
-// various helper macros that ease development for windows
+//  various helper macros that ease development for windows
 #if defined(ZENO_WINDOWS)
 #    if defined(interface)
 #        undef interface
 #    endif
 //  a useful macro that allows for easy release of com objects with checking and nullifying
 //  typical usage: destructors
-#    define SAFE_COM_RELEASE(interface)    \
-        {                             \
-            if (interface)            \
-            {                         \
-                interface->Release(); \
-                interface = nullptr;  \
-            }                         \
+#    define SAFE_COM_RELEASE(interface) \
+        {                               \
+            if (interface)              \
+            {                           \
+                interface->Release();   \
+                interface = nullptr;    \
+            }                           \
         }
 
+#    define interface __STRUCT__
 #endif
 
 #if !defined(ZENO_LIB)
