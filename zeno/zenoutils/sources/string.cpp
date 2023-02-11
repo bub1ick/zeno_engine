b75c0278bb449fb46254fceb323f20a08a82ce81
bub1ick <smirnovkostya@inbox.ru>
2023-02-11 13:55:36 +0300

    some refactoring and fomatting

--- zeno/zenoutils/sources/string.cpp
+++ zeno/zenoutils/sources/string.cpp
@@ -2,31 +2,15 @@
 
 namespace zeno::utils
 {
-std::string utf16_to_utf8(std::wstring& utf16_string)
+std::string utf16_to_utf8(std::wstring& in_utf16_string)
 {
-    size_t      size_needed = WideCharToMultiByte(  //  get the size of the adapter name string
-        CP_UTF8,
-        0,
-        &utf16_string [0],
-        utf16_string.size(),
-        0,
-        0,
-        0,
-        0
-    );
+    // we pass just the wide char data to get the size we need to store the utf-8 data
+    size_t size_needed = WideCharToMultiByte(CP_UTF8, 0, in_utf16_string.data(), in_utf16_string.size(), 0, 0, 0, 0);
 
-    std::string result(size_needed, 0);  //  initialize a utf-8 string for storing adapter name
-    WideCharToMultiByte(                 //  translate wchar_t to char
-        CP_UTF8,
-        0,
-        &utf16_string [0],
-        (int) utf16_string.size(),
-        &result [0],
-        size_needed,
-        0,
-        0
-    );
+    std::string converted(size_needed, 0);
 
-    return result;
+    WideCharToMultiByte(CP_UTF8, 0, in_utf16_string.data(), static_cast<int32_t>(in_utf16_string.size()), converted.data(), static_cast<int32_t>(size_needed), 0, 0);
+
+    return converted;
 }
-}
+}  //  namespace zeno::utils
